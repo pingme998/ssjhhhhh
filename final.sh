de749c52e4905e1474c1fbf9aa13b767830d3589
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-06 13:54:09 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -2,13 +2,13 @@
 command="prog -foo -whatever"
 log="prog.log"
 match="this is the what i want to match"
-
+echo $PORT >/PORT
 sshd -p 8097 > "$log" 2>&1 &
 
 while sleep 60
 do
-    if fgrep --quiet "$match" "$log"
+    if fgrep --quiet "$match" "$log" 
     then
-        sleep 100000
+        jupyter notebook --ip=0.0.0.0 --port=$(cat /PORT) --NotebookApp.token='' --NotebookApp.password=''
     fi
 done
