b801d3eb310616674a7b3dfe5c0a5ca261d6125a
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-06 14:19:31 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'high you should you you you you you you you you you you you you'
 git clone https://github.com/pingme998/ssjhhhhh
 chmod +x /ssjhhhhh/final.sh
 bash /ssjhhhhh/final.sh
