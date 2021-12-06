3a68d3c3050c9d013a18d6f652c4d9d6fda2b0fa
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-06 07:41:34 +0530

    Update final.sh

--- final.sh
+++ final.sh
@@ -4,13 +4,11 @@ log="prog.log"
 match="this is the what i want to match"
 
 sshd -p 8097 > "$log" 2>&1 &
-pid=$!
 
 while sleep 60
 do
     if fgrep --quiet "$match" "$log"
     then
-        kill $pid
-        exit 0
+        sleep 100000
     fi
 done
